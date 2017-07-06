5e8a4288f2d1da8fa8413f344a4a5f1351386771
jamebluntcc <291552579@qq.com>
2017-07-06 18:10:01 +0800

    rm test files

--- tests/test_helloworld.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-context("test helloworld")
-test_that("test helloworld",{
-  printstr <- "hi chencheng"
-  expect_that(sayHi("chencheng"),equals(printstr))
-})
-#single file unittest
-
