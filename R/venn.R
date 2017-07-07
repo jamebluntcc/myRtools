68d220d29b9655722802b280a75f512bb0bfb3a1
jamebluntcc <291552579@qq.com>
2017-07-07 16:49:40 +0800

    add savetype and margin params in venn

--- R/venn.R
+++ R/venn.R
@@ -1,12 +1,14 @@
 #' plot a venn graphics
 #' @description plot a venn graphics it wrap VennDiagram and make venn more simple
-#' @author chencheng <jamebluntxy@gmail.com>
-#' @param data A list for your group data
-#' @param fillColor A vector for fill your venn plot default \code{fillColor} is provided
-#' @param fileName A character for your outputfile name
-#' @param main A character for the title in your venn plot default \code{main} is NULL
-#' @param fontSize A number for your label size default \code{fontSize} is 1.5
-#' @param fontColor A character for your label color default \code{fontColor} is white
+#' @author chencheng <chencheng@onmath.cn>
+#' @param data a list for your group data
+#' @param fillColor a vector for fill your venn plot default \code{fillColor} is provided
+#' @param fileName a character for your outputfile name
+#' @param main a character for the title in your venn plot default \code{main} is NULL
+#' @param fontSize a number for your label size default \code{fontSize} is 1.5
+#' @param fontColor a character for your label color default \code{fontColor} is white
+#' @param saveType a character for your save file type png or tiff
+#' @param Margin a number for your venn margin more Margin less size in your venn graphics
 #' @return a tiff or png file for your venn graphics
 #' @note Since the \code{venn.diaram()} is not support pdf type file so your \code{fillName} not allow pdf type file
 #' @examples
@@ -20,15 +22,22 @@
 #' venn_plot(data=list(A=GroupA,B=GroupB,C=GroupC,D=GroupD),fileName='test_venn_plot.tiff')
 #' @export
 #'
-venn_plot <- function(data,fillColor=NULL,fileName,main=NULL,fontSize=1.5,fontColor='white'){
+venn_plot <- function(data,fillColor=NULL,fileName=NULL,main=NULL,fontSize=1.5,fontColor='white',saveType = c("tiff","png"),Margin = 0.1){
   if(is.null(fillColor)){
     defaultColor <- c("#386cb0","#fdb462","#7fc97f","#ef3b2c")
     dataNum <- length(data)
     fillColor <- defaultColor[1:dataNum]
   }
+  if(is.null(fileName)){
+    venn_file <- 'venn'
+  }else{
+    venn_file <- fileName
+  }
+  savetype <- match.arg(saveType)
+
   VennDiagram::venn.diagram(
     x = data,
-    filename = fileName,
+    filename = paste(fileName,savetype,sep = "."),
     col = "transparent",
     fill = fillColor,
     main = main,
@@ -39,6 +48,6 @@ venn_plot <- function(data,fillColor=NULL,fileName,main=NULL,fontSize=1.5,fontCo
     cat.col=fillColor,
     cat.fontfamily="serif",
     rotation.degree = 0,
-    margin = 0.1
+    margin = Margin
   )
 }
