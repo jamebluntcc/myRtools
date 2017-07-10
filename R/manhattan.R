557b20d0a444f29bce6d89f48b0dd623eae0f76f
jamebluntcc <291552579@qq.com>
2017-07-10 14:09:26 +0800

    add filePath params

--- R/manhattan.R
+++ R/manhattan.R
@@ -2,7 +2,7 @@
 #'
 manhattan_plot <- function(data,Chr="CHR",Bp="BP",P="p",Snp="SNP",AnnotatePval = NULL,AnnotateTop = TRUE,
                            Col=NULL,ChrLabs = NULL,SuggestiveLine = -log10(5e-05),GenomeWideLine = -log10(5e-08),
-                           Highlight = NULL,Logp = TRUE,fileName = NULL,width=8,height=6,saveType = c('both','png','pdf'),...){
+                           Highlight = NULL,Logp = TRUE,fileName = NULL,filePath = "",width=8,height=6,saveType = c('both','png','pdf'),...){
   if(is.null(Col)){
     manhatton_col = RColorBrewer::brewer.pal("Set1",n=9)
   }
@@ -14,7 +14,7 @@ manhattan_plot <- function(data,Chr="CHR",Bp="BP",P="p",Snp="SNP",AnnotatePval =
   save_type <- match.arg(saveType)
 
   if(save_type == 'both'){
-    png(filename = paste0(manhattan_file,'.png'),width = width,height = height,res = 300,type = "cairo",units = "in")
+    png(filename = paste0(filePath,manhattan_file,'.png'),width = width,height = height,res = 300,type = "cairo",units = "in")
     qqman::manhattan(na.omit(data),chr=Chr,bp=Bp,p=P,snp=Snp,
                      annotatePval=AnnotatePval,annotateTop=AnnotateTop,
                      col=manhatton_col,chrlabs=ChrLabs,
